835467d9c67b60adc20e3c3172499b7a77749bd9
Abdulrhman <abdulrhmangoni@gmail.com>
2023-12-18 19:15:39 +0300

    Add `style` prop to `FetchFailedAlert` component
    - Export `Typography` component of "mui" directly in `P.tsx` file
    instead of recieve its props as props of the custom `P` component.

--- src/components/FetchFailedAlert/FetchFailedAlert.tsx
+++ src/components/FetchFailedAlert/FetchFailedAlert.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Refresh } from "@mui/icons-material";
-import { Alert, IconButton } from "@mui/material";
+import { Alert, IconButton, SxProps } from "@mui/material";
 
 interface FetchFailedAlertProps {
     message: string,
     refetch?: () => void,
     height?: string,
     width?: string,
+    style?: SxProps
 }
 
-export default function FetchFailedAlert({ message, refetch, height = "100%", width = "100%" }: FetchFailedAlertProps) {
+export default function FetchFailedAlert(props: FetchFailedAlertProps) {
+
+    const { message, refetch, height = "100%", width = "100%", style } = props;
     return (
         <Alert
             severity="error"
             className="flex-row-center"
             sx={{
                 height, width,
-                "& .MuiAlert-action": { ml: 0, p: "0px 0 0 8px" }
+                "& .MuiAlert-action": { ml: 0, p: "0px 0 0 8px" },
+                ...style
             }}
             action={
                 refetch &&
--- src/components/P/P.tsx
+++ src/components/P/P.tsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import { Typography, TypographyOwnProps } from '@mui/material'
+import { Typography } from '@mui/material'
 
-export interface PProps extends TypographyOwnProps { className?: string }
-
-export default function P(props: PProps) {
-    return (
-        <Typography {...props} />
-    )
-}
+export default Typography
