533c6d2b48b31b9efcf0f61892dcd03c071cbc37
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-02-16 23:55:39 +0300

    Add more options to `useHTTPRequestState` hook - Add setting initial values option for all states. - Add `data` state

--- src/hooks/useHTTPRequestState/useHTTPRequestState.tsx
+++ src/hooks/useHTTPRequestState/useHTTPRequestState.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 
-export default function useHTTPRequestState() {
+type StateOptions = {
+    initialLoading: boolean,
+    initialError: boolean,
+    initialSuccess: boolean,
+}
 
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [isError, setIsError] = useState<boolean>(false);
-    const [isSuccess, setIsSuccess] = useState<boolean>(false);
+export default function useHTTPRequestState<T>(initialData?: T, options?: StateOptions) {
+
+    const [isLoading, setIsLoading] = useState<boolean>(options?.initialLoading || false);
+    const [isError, setIsError] = useState<boolean>(options?.initialError || false);
+    const [isSuccess, setIsSuccess] = useState<boolean>(options?.initialSuccess || false);
+    const [data, setData] = useState<T | undefined>(initialData);
 
     return {
+        data,
+        setData,
         isLoading,
         setIsLoading,
         isSuccess,
